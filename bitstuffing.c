9b7b84464ae652f242ccffa7912c11d3fbf21c68
RAV <92453425+RAVBLACK@users.noreply.github.com>
2026-06-21 20:39:13 +0530

    bitstuffing in C

---
+++ bitstuffing.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void bitStuffing(int N, int arr[]) {
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        printf("%d", arr[i]);
+        if (arr[i] == 1) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count == 5) {
+            printf("0");
+            count = 0; 
+        }
+    }
+    printf("\n");
+}
+int main() {
+    int arr[] = {1, 1, 1, 1, 1, 1};
+    bitStuffing(6, arr);
+    return 0;
+}
